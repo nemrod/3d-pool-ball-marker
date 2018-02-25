2a654537c9cf0d8a5ce72b3fb5ec83437eb373d2
Anders Mårtensson <anders@nemrod.se>
2018-02-25 01:35:15 +0100

    added readme

---
+++ README.mk
@@ -0,0 +1,5 @@
+# Pool Ball Position Marker
+
+Position marker for pool/billiard/snooker balls. Put it on the table against the ball so the ball can be retrieved and then replaced in the same position again, for example for cleaning the ball or so that a magic rack can be removed from under it. The STL is rendered for the standard international pool ball size (57.2mm), the customizer can be used to set a different size (the whole model will scale). British pool uses smaller balls, 51-56mm, with an even smaller cue ball (from 48mm). Snooker uses balls that are 52.5mm.
+
+Published on Thingiverse [here](https://www.thingiverse.com/thing:2735678).
